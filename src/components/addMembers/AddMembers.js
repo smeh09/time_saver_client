c17ea6a77156308b56b550eed70facdc1e04d8fd
Shaurya <87943678+smeh09@users.noreply.github.com>
2022-01-10 01:11:23 -0800

    navigate warning

--- src/components/addMembers/AddMembers.js
+++ src/components/addMembers/AddMembers.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { Navigate, useParams } from "react-router-dom";
+import { useParams } from "react-router-dom";
 
 const AddMembers = () => {
   const [email, setEmail] = useState("");
