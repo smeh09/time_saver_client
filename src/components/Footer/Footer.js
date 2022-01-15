6f9d9d2e1a9520e5f8ab2daf929bce67e84729ff
Shaurya <87943678+smeh09@users.noreply.github.com>
2022-01-15 03:48:24 -0800

    finished!!

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -3,14 +3,6 @@ import "./Footer.css";
 
 export default function Footer() {
   return (
-    <footer className="footer">
-      &copy; 2022-2023 Made by &nbsp;{" "}
-      <a
-        style={{ color: "rgb(116, 153, 255)" }}
-        href="https://github.com/smeh09"
-      >
-        Smeh09
-      </a>
-    </footer>
+    <footer className="footer">&copy; 2022-2023 Made by Shaurya Mehta</footer>
   );
 }
